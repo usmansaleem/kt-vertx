0d1db9a8228e44713dcfae9ce72a7f8804acfbf9
Usman Saleem <usman.saleem@flightcentre.com.au>
2017-11-16 16:17:59 +1000

    Add path when redirecting to ssl

--- src/main/java/info/usmans/blog/vertx/Extensions.kt
+++ src/main/java/info/usmans/blog/vertx/Extensions.kt
@@ -4,16 +4,21 @@ import io.vertx.core.http.HttpServerRequest
 import java.net.URI
 
 fun HttpServerRequest.redirectToSecure(publicHttpsPort: Int = 443, redirectCode: Int = 302) {
-     val hostName: String = if(this.absoluteURI().isNullOrBlank())
-         "usmans.info"
-     else
-         URI(this.absoluteURI()).host ?: "usmans.info"
+    val hostName: String = if (this.absoluteURI().isNullOrBlank())
+        "usmans.info"
+    else
+        URI(this.absoluteURI()).host ?: "usmans.info"
 
-    val path:String = if(publicHttpsPort == 443) "https://${hostName}" else "https://${hostName}:${publicHttpsPort}"
-    this.response().putHeader("location", path).setStatusCode(redirectCode).end()
+    val path: String = if (this.absoluteURI().isNullOrBlank())
+        ""
+    else
+        URI(this.absoluteURI()).path ?: ""
+
+    val url: String = if (publicHttpsPort == 443) "https://${hostName}${path}" else "https://${hostName}:${publicHttpsPort}${path}"
+    this.response().putHeader("location", url).setStatusCode(redirectCode).end()
 }
 
 fun HttpServerRequest.getOAuthRedirectURI(path: String): String {
-    val redirectURI = URI(absoluteURI()?: "https://usmans.info")
+    val redirectURI = URI(absoluteURI() ?: "https://usmans.info")
     return "https://" + redirectURI.authority + path
 }
