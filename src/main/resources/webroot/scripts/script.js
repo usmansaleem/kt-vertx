f884dc200b960d8bba487eabcb41428f976c5339
Usman Saleem <usman.saleem@flightcentre.com.au>
2017-11-18 12:11:46 +1000

    Removing ezfb module

--- src/main/resources/webroot/scripts/script.js
+++ src/main/resources/webroot/scripts/script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // create the module and name it blogApp
-var blogApp = angular.module('blogApp', ['ngRoute','ngAnimate','ui.bootstrap', 'ngSanitize', 'viewhead', 'ezfb']);
+var blogApp = angular.module('blogApp', ['ngRoute','ngAnimate','ui.bootstrap', 'ngSanitize', 'viewhead']);
 
 //ng-routes
 blogApp.config(function($routeProvider) {
